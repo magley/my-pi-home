1e2d28f6527858fcdab068101b27f2fe54fc3fbd
elkcert <80402248+elkcert@users.noreply.github.com>
2024-01-20 20:14:52 +0100

    PI3 devices/dashboards + remaining PI2 dashboards

--- frontend/mypihome/src/components/Charts.vue
+++ frontend/mypihome/src/components/Charts.vue
@@ -7,43 +7,78 @@
 <!-- Alarm -->
 <iframe src="http://localhost:3000/d-solo/a6c16b56-5d85-4495-a45a-039c57b3f39e/alarms?orgId=1&refresh=5s&panelId=1" width="450" height="200" frameborder="0"></iframe>
 
-<!-- Buzzer -->
+<h3>Pi 1</h3>
+
+<!-- DS1 -->
+<iframe src="http://localhost:3000/d-solo/f48172bf-2863-4f0c-9463-335addb9bf42/mds?orgId=1&from=1704547902066&to=1704569502067&panelId=1" width="450" height="200" frameborder="0"></iframe>
+
+<!-- DL -->
+<iframe src="http://localhost:3000/d-solo/cda101da-b1da-4cdf-b16c-2d4a04d1d83a/led?orgId=1&from=1702927164938&to=1704569449897&panelId=1" width="450" height="200" frameborder="0"></iframe>
+
+<!-- DUS1 -->
+<iframe src="http://localhost:3000/d-solo/c501920a-ea78-4a7e-ab0e-7df6ea032def/uds?orgId=1&from=1704547972221&to=1704566295304&panelId=1" width="450" height="200" frameborder="0"></iframe>
+
+<!-- DB -->
 <iframe src="http://localhost:3000/d-solo/bbba139c-497d-4053-ab4a-df03cb589de8/buzzer?orgId=1&refresh=5s&panelId=1" width="450" height="200" frameborder="0"></iframe>
+
+<!-- DPIR1 -->
+<iframe src="http://localhost:3000/d-solo/d1b939a4-abce-4f5f-8be3-0fb80ad7da01/pir?orgId=1&from=1702478219036&to=1702483244828&panelId=3" width="450" height="200" frameborder="0"></iframe>
+
+<!-- DMS -->
+<iframe src="http://localhost:3000/d-solo/bf89ea0d-619e-4de7-a13a-176bad3c62a6/mbkp?orgId=1&refresh=5s&from=1704547874562&to=1704569474562&panelId=1" width="450" height="200" frameborder="0"></iframe>
+
+<!-- RPIR1 -->
+<iframe src="http://localhost:3000/d-solo/d1b939a4-abce-4f5f-8be3-0fb80ad7da01/pir?orgId=1&from=1702478219036&to=1702483244828&panelId=1" width="450" height="200" frameborder="0"></iframe>
+<!-- RPIR2 -->
+<iframe src="http://localhost:3000/d-solo/d1b939a4-abce-4f5f-8be3-0fb80ad7da01/pir?orgId=1&from=1702478219036&to=1702483244828&panelId=2" width="450" height="200" frameborder="0"></iframe>
 
 <!-- RDHT 1 -->
 <iframe src="http://localhost:3000/d-solo/c9baac51-8a62-4371-98d7-3dd8b120e979/dht?orgId=1&refresh=2s&panelId=2" width="450" height="200" frameborder="0"></iframe>
 <!-- RDHT 2 -->
 <iframe src="http://localhost:3000/d-solo/c9baac51-8a62-4371-98d7-3dd8b120e979/dht?orgId=1&orgId=1&refresh=2s&panelId=3" width="450" height="200" frameborder="0"></iframe>
 
+<h3>Pi 2</h3>
+<!-- DS2 -->
+<iframe src="http://localhost:3000/d-solo/f48172bf-2863-4f0c-9463-335addb9bf42/mds?orgId=1&from=1705754376457&to=1705775976457&panelId=2" width="450" height="200" frameborder="0"></iframe>
+
+<!-- DUS2 -->
+<iframe src="http://localhost:3000/d-solo/c501920a-ea78-4a7e-ab0e-7df6ea032def/uds?orgId=1&from=1705754438568&to=1705776038568&panelId=2" width="450" height="200" frameborder="0"></iframe>
+
+<!-- DPIR2 -->
+<iframe src="http://localhost:3000/d-solo/d1b939a4-abce-4f5f-8be3-0fb80ad7da01/pir?orgId=1&refresh=5s&from=1705775946839&to=1705776246839&panelId=4" width="450" height="200" frameborder="0"></iframe>
+
+<!-- GDHT -->
+<iframe src="http://localhost:3000/d-solo/c9baac51-8a62-4371-98d7-3dd8b120e979/dht?orgId=1&refresh=5s&from=1705776086988&to=1705776386988&panelId=4" width="450" height="200" frameborder="0"></iframe>
+
+<!-- GLCD -->
+<iframe src="http://localhost:3000/d-solo/cc6c338f-6e92-4bcd-a4d0-562f1480c24f/lcd?orgId=1&from=1704547845922&to=1704569445922&panelId=1" width="450" height="200" frameborder="0"></iframe>
+
 <!-- Gyroscope acceleration -->
 <iframe src="http://localhost:3000/d-solo/c1d4c66e-d278-4f72-813c-fdc56dbf97cb/gyroscope?orgId=1&from=1704569110888&to=1704569410888&panelId=1" width="450" height="200" frameborder="0"></iframe>
 <!-- Gyroscope rotation -->
 <iframe src="http://localhost:3000/d-solo/c1d4c66e-d278-4f72-813c-fdc56dbf97cb/gyroscope?orgId=1&from=1704569134795&to=1704569434795&panelId=2" width="450" height="200" frameborder="0"></iframe>
 
-<!-- LCD -->
-<iframe src="http://localhost:3000/d-solo/cc6c338f-6e92-4bcd-a4d0-562f1480c24f/lcd?orgId=1&from=1704547845922&to=1704569445922&panelId=1" width="450" height="200" frameborder="0"></iframe>
+<!-- RPIR3 -->
+<iframe src="http://localhost:3000/d-solo/d1b939a4-abce-4f5f-8be3-0fb80ad7da01/pir?orgId=1&refresh=5s&from=1705776549171&to=1705776849171&panelId=5" width="450" height="200" frameborder="0"></iframe>
 
-<!-- LED -->
-<iframe src="http://localhost:3000/d-solo/cda101da-b1da-4cdf-b16c-2d4a04d1d83a/led?orgId=1&from=1702927164938&to=1704569449897&panelId=1" width="450" height="200" frameborder="0"></iframe>
+<!-- RDHT3 -->
+<iframe src="http://localhost:3000/d-solo/c9baac51-8a62-4371-98d7-3dd8b120e979/dht?orgId=1&refresh=5s&from=1705776657481&to=1705776957481&panelId=5" width="450" height="200" frameborder="0"></iframe>
 
-<!-- Membrane keypad -->
-<iframe src="http://localhost:3000/d-solo/bf89ea0d-619e-4de7-a13a-176bad3c62a6/mbkp?orgId=1&refresh=5s&from=1704547874562&to=1704569474562&panelId=1" width="450" height="200" frameborder="0"></iframe>
+<h3>Pi 3</h3>
+<!-- RPIR4 -->
+<iframe src="http://localhost:3000/d-solo/d1b939a4-abce-4f5f-8be3-0fb80ad7da01/pir?orgId=1&refresh=5s&from=1705776745817&to=1705777045817&panelId=6" width="450" height="200" frameborder="0"></iframe>
 
-<!-- Door sensor -->
-<iframe src="http://localhost:3000/d-solo/f48172bf-2863-4f0c-9463-335addb9bf42/mds?orgId=1&from=1704547902066&to=1704569502067&panelId=1" width="450" height="200" frameborder="0"></iframe>
+<!-- RDHT4 -->
+<iframe src="http://localhost:3000/d-solo/c9baac51-8a62-4371-98d7-3dd8b120e979/dht?orgId=1&refresh=5s&from=1705776764232&to=1705777064232&panelId=6" width="450" height="200" frameborder="0"></iframe>
 
-<!-- RPIR1 -->
-<iframe src="http://localhost:3000/d-solo/d1b939a4-abce-4f5f-8be3-0fb80ad7da01/pir?orgId=1&from=1702478219036&to=1702483244828&panelId=1" width="450" height="200" frameborder="0"></iframe>
-<!-- RPIR2 -->
-<iframe src="http://localhost:3000/d-solo/d1b939a4-abce-4f5f-8be3-0fb80ad7da01/pir?orgId=1&from=1702478219036&to=1702483244828&panelId=2" width="450" height="200" frameborder="0"></iframe>
-<!-- DPIR1 -->
-<iframe src="http://localhost:3000/d-solo/d1b939a4-abce-4f5f-8be3-0fb80ad7da01/pir?orgId=1&from=1702478219036&to=1702483244828&panelId=3" width="450" height="200" frameborder="0"></iframe>
-
-<!-- DUS1 -->
-<iframe src="http://localhost:3000/d-solo/c501920a-ea78-4a7e-ab0e-7df6ea032def/uds?orgId=1&from=1704547972221&to=1704566295304&panelId=1" width="450" height="200" frameborder="0"></iframe>
+<!-- BB -->
+<iframe src="http://localhost:3000/d-solo/bbba139c-497d-4053-ab4a-df03cb589de8/buzzer?orgId=1&from=1702926149457&to=1702926259056&panelId=2" width="450" height="200" frameborder="0"></iframe>
 
 <!-- B4SD -->
 <iframe src="http://localhost:3000/d-solo/b240d08b-fc18-4bf9-a143-23f2348240fa/d4s7?orgId=1&from=1705749771329&to=1705771371329&panelId=1" width="450" height="200" frameborder="0"></iframe>
+
+<!-- BIR -->
+<iframe src="http://localhost:3000/d-solo/d1b939a4-abce-4f5f-8be3-0fb80ad7da01/pir?orgId=1&refresh=5s&from=1705776950529&to=1705777250529&panelId=7" width="450" height="200" frameborder="0"></iframe>
 
 <!-- BRGB -->
 <iframe src="http://localhost:3000/d-solo/a06505f1-0f0e-4fd0-8d25-6c51417d0aca/rgb?orgId=1&from=1705752639460&to=1705774239461&panelId=1" width="450" height="200" frameborder="0"></iframe>
